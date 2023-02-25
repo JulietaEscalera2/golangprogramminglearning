be76b889cd1bbced488e3f838857dacaa86b0e14
Kevin Sanchez <kevin.sanchezpeep@gmail.com>
2023-02-24 20:00:04 -0400

    added just hello program (#1)
    * added just hello program
    
    * added new comment

---
+++ hello.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func PrintHello() {
+	fmt.Println("Hello, Modules! This is mypackage speaking!")
+	fmt.Println("Hello, Modules! This is mypackage speaking! second line")
+}
