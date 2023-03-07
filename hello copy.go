3ce2051445f5c66b9e81303cfbf576caa3cb0280
Kevin <kevin.sanchezpeep@gmail.com>
2023-03-06 20:28:56 -0400

    added changed for method and messages

--- hello copy.go
+++ hello copy.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func PrintHello() {
+func PrintHelloNewMessage() {
 	fmt.Println("Hello, Modules! 123 my changes")
 	fmt.Println("Hello, his is mypackage speaking! second line")
 }
